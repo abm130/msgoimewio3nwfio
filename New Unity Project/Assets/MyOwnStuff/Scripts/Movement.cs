02b0e6bc1371e2438c7c176c4720fe28cd3516ef
abm130 <waldekjulez@gmail.com>
2017-02-24 01:35:16 +0100

    Diverse Bewegungsskripte erstellt und Objekten zugewiesen. Raumschiff erweitert und texturiert. Planetenbeleuchtung geändert, Atmosphere für den Planeten erstellt. Sonne erstellen hatte nicht funktioniert.

---
+++ New Unity Project/Assets/MyOwnStuff/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float horizontalSpeed;
+	public float verticalSpeed;
+	public float amplitude;
+	public Vector3 tempPosition;
+
+	// Use this for initialization
+	void Start () {
+		tempPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		tempPosition.z += horizontalSpeed;
+		tempPosition.y = Mathf.Sin (Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+		transform.position = tempPosition;
+	}
+}
---
+++ New Unity Project/Assets/MyOwnStuff/Scripts/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+	public float xspeed = 0.05f;
+	public float yspeed = 0.05f;
+	public float zspeed = 0.05f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (xspeed, yspeed, zspeed);
+	}
+}
